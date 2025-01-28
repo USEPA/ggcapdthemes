a97a6bc25a9013542402fae3317d99894ce9f2d6
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2025-01-28 13:04:11 -0500

    update theme_capd settings for gridlines, font sizing, margins, and y-axis title

--- R/theme_capd.R
+++ R/theme_capd.R
@@ -17,7 +17,7 @@
 #'       caption = 'Data from 2022 eGRID summary data, Table 2')
 #' p_bar + theme_capd()
 
-theme_capd <- function(){
+theme_capd <- function(base_size=11, orientation = "horiz"){
 
   if(!('Source Sans 3' %in% sysfonts::font_families())){
     sysfonts::font_add_google('Source Sans 3')
@@ -34,13 +34,17 @@ theme_capd <- function(){
 
   ## use default font until sysfonts kinks are worked out
   base_family <- 'Source Sans 3'
+  half_line <- base_size/2
   th <- list(
-    ggplot2::theme_bw() %+replace%
+    ggplot2::theme_bw(base_size=base_size) %+replace%
       theme(
+        text = element_text(family = base_family, face = "plain",
+                                    colour = "black", size = base_size),
+
         ## format title, subtitle, caption
-        plot.title= element_text(hjust = 0.5, family = base_family),
-        plot.subtitle = element_text(hjust = 0.5, family = base_family),
-        plot.caption = element_text(family = base_family),
+        plot.title= element_text(hjust = 0, size = rel(1.2), family = base_family, margin = margin(b = half_line)),
+        plot.subtitle = element_text(hjust = 0, family = base_family, margin = margin(b = half_line)),
+        plot.caption = element_text(family = base_family, size = rel(0.8)),
 
         ## format plot background
         ## change background color
@@ -49,20 +53,33 @@ theme_capd <- function(){
         panel.border = element_blank(),
 
         ## gridlines
-        panel.grid = element_line(color = '#c9c9c9',linetype = 'dotted'),
+        panel.grid = element_line(color = '#c9c9c9',linetype = 'dotted',linewidth = rel(1)),
+        panel.grid.minor = element_line(color = '#c9c9c9',linetype = 'dotted',linewidth = rel(1)),
         axis.line = element_line(color = '#2e2e2e', linetype = 'solid'),
 
         ## format axes
         axis.title = element_text(family = base_family),
-        axis.title.y = element_text(vjust = 1, angle=0,hjust = 1),#,margin=margin(l=0,r=-100)),
+        #axis.title.y = element_text(vjust = 1, angle=0,hjust = 1),#,margin=margin(l=0,r=-100)),
         axis.ticks = element_blank(),
-        axis.text = element_text(family = base_family),
+        axis.text = element_text(family = base_family, size=rel(0.8)),
         ## format legend
-        legend.text = element_text(family = base_family),
-        legend.title = element_text(family = base_family),
+        legend.text = element_text(family = base_family, size = rel(0.8)),
+        legend.title = element_text(family = base_family, ),
 
       )
+
   )
+
+  if(orientation == "horiz"){
+    th[[1]]$panel.grid.major.x <- element_blank()
+    th[[1]]$panel.grid.minor.x <- element_blank()
+
+  } else if (orientation == "vert"){
+    th[[1]]$panel.grid.major.y <- element_blank()
+    th[[1]]$panel.grid.minor.y <- element_blank()
+
+  }
+
 
   showtext::showtext_end()
 
