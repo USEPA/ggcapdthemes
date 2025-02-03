f487221bdfdd61b64a27c9b324c2dcd775517aea
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2025-02-03 16:36:47 -0500

    add combustion and renewable example datasets

---
+++ data-raw/preprocess_renewables_and_combustion.R
@@ -0,0 +1,27 @@
+
+renewables_raw <- read.csv('data-raw/Resource Mix by renewable vs. non-renewable fuels, by eGRID subregion, 2023.csv') |>
+  janitor::clean_names()
+
+renewables <- renewables_raw |>
+  dplyr::mutate(hydro = readr::parse_number(hydro),
+          all_non_hydro_renewables = readr::parse_number(all_non_hydro_renewables),
+         all_non_renewables = readr::parse_number(all_non_renewables),
+         region = factor(region, levels=region)) |>
+  tidyr::pivot_longer(cols=hydro:all_non_renewables, names_to='fuel_type', values_to='resource_mix') |>
+  # mutate(fuel_type = str_replace_all(fuel_type, c('hydro' = 'Hydro', 'all_non_hydro_renewables'='Non-hydro', 'all_non_renewables'='Non-nuclear'))) %>%
+  dplyr::mutate(fuel_type = stringr::str_replace_all(fuel_type, c('all_non_hydro_renewables'='Renewable', 'all_non_renewables'='Non-renewable','hydro' = 'Renewable')))
+
+usethis::use_data(renewables, overwrite = TRUE)
+
+
+combustion_raw <- read.csv('data-raw/Resource Mix by combustible vs. non-combustible fuels, by eGRID subregion, 2023.csv') |>
+  janitor::clean_names()
+
+combustion <- combustion_raw |>
+  dplyr::mutate(all_combustion = readr::parse_number(all_combustion),
+         all_non_combustion = readr::parse_number(all_non_combustion),
+         region = factor(region, levels=region)) |>
+  tidyr::pivot_longer(cols=all_combustion:all_non_combustion, names_to='fuel_type', values_to='resource_mix') |>
+  dplyr::mutate(fuel_type = stringr::str_replace_all(fuel_type, c( 'all_combustion'='Combustion', 'all_non_combustion'='Non-combustion')))
+
+usethis::use_data(combustion, overwrite = TRUE)
