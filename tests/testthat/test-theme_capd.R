5305c724963482157f03c74760c2808974b82c3d
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2025-02-07 17:12:16 -0500

    cleanup .onLoad function, fix unit test typo

--- tests/testthat/test-theme_capd.R
+++ tests/testthat/test-theme_capd.R
@@ -3,7 +3,7 @@ test_that("theme_capd defaults works", {
   th <- theme_capd()[[1]]
 
   ## test that theme is correct object type
-  expect_s3_class(th1, "theme")
+  expect_s3_class(th, "theme")
 
   ## test that font is passed on properly
   expect_equal(th$text$family, "Source Sans 3")
