198d49cc9c31e28a63d7ea8ff13e561b1da8fe5d
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2025-02-10 16:54:51 -0500

    add unit test for theme relative font sizing

--- tests/testthat/test-theme_capd.R
+++ tests/testthat/test-theme_capd.R
@@ -11,3 +11,19 @@ test_that("theme_capd defaults works", {
   ## test that default font size is 11
   expect_equal(th$text$size, 11)
 })
+
+test_that("theme_capd font sizes adjust", {
+
+  size_sm <- 4
+  th <- theme_capd(base_size = size_sm)[[1]]
+
+  ## test that default font size is 4
+  expect_equal(th$text$size, size_sm)
+
+  ## test that title relative font size is 1.2
+  expect_equal(as.numeric(th$plot.title$size) * as.numeric(th$text$size), size_sm * 1.2)
+
+  ## test that title relative font size is 1.2
+  expect_equal(as.numeric(th$plot.title$size) * as.numeric(th$text$size), size_sm * 1.2)
+
+})
