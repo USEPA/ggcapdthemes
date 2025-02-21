d89a462fe00b497524de22a80c5d49838617e990
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2025-02-21 16:08:17 -0500

    add grDevices namespace to windows onLoad function

--- R/zzz.R
+++ R/zzz.R
@@ -4,10 +4,10 @@
 .onLoad <- function(libname, pkgname) {
 
   if (Sys.info()[1] == "Windows") {
-    windowsFonts()
+    grDevices::windowsFonts()
     suppressMessages(extrafont::font_import(paths = system.file("fonts/Source_Sans_3", package="ggcapdthemes"), prompt = FALSE))
     extrafont::loadfonts(quiet = TRUE)
-    windowsFonts()
+    grDevices::windowsFonts()
   } else {
     suppressMessages(extrafont::font_import(paths = system.file("fonts/Source_Sans_3", package="ggcapdthemes"), prompt = FALSE))
     extrafont::loadfonts(quiet = TRUE)
