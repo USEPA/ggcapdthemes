f3f04fd5b731b20c85e33d157588794d3436ab06
Marschall Furman <51181259+mlfurman3@users.noreply.github.com>
2025-07-15 16:16:17 -0400

    update .onLoad function to import OTF font files using systemfonts package
    Note: The extrafonts package does not work with OTF files, only TTF

--- R/zzz.R
+++ R/zzz.R
@@ -3,13 +3,25 @@
 ## goal: install and load Sans source 3 fonts from inst/fonts folder
 .onLoad <- function(libname, pkgname) {
 
-  if (Sys.info()[1] == "Windows") {
-    grDevices::windowsFonts()
-    suppressMessages(extrafont::font_import(paths = system.file("fonts/Source_Sans_3", package="ggcapdthemes"), prompt = FALSE))
-    extrafont::loadfonts(quiet = TRUE)
-    grDevices::windowsFonts()
-  } else {
-    suppressMessages(extrafont::font_import(paths = system.file("fonts/Source_Sans_3", package="ggcapdthemes"), prompt = FALSE))
-    extrafont::loadfonts(quiet = TRUE)
+    ## load OTF font files
+    systemfonts::add_fonts(files = list.files(system.file("fonts/Source_Sans_3/static/", package="ggcapdthemes"), full.names = TRUE))
+
+    if(!("Source Sans 3" %in% systemfonts::system_fonts()$family)){
+      systemfonts::register_font(
+        'Source Sans 3',
+        plain = 'inst/fonts/Source_Sans_3/static/SourceSans3-Regular.otf',
+        bold = 'inst/fonts/Source_Sans_3/static/SourceSans3-Bold.otf',
+        italic = 'inst/fonts/Source_Sans_3/static/SourceSans3-It.otf',
+        bolditalic = 'inst/fonts/Source_Sans_3/static/SourceSans3-BoldIt.otf'
+      )
+    }
+    ## add variant for SALT styling of letters
+    systemfonts::register_variant(
+      name = "Source Sans 3 Salt",
+      family = "Source Sans 3",
+      features = systemfonts::font_feature(
+        letters = "stylistic"
+      )
+    )
+
   }
-}
